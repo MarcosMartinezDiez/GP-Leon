30fad5fdceaccfda139587e8a0ded38a91441a6b
Marcos <martinezdiezmarcos7@gmail.com>
2023-04-25 12:28:15 +0200

    PRIMER COMMIT

---
+++ Circuito.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Circuito here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Circuito
+{
+    // instance variables - replace the example below with your own
+    private String nombreCircuito;
+    
+    private int numeroCurvas;
+    /**
+     * Constructor for objects of class Circuito
+     */
+    public Circuito(String nombreCircuito, int numeroCurvas)
+    {
+        // initialise instance variables
+        String nombreCirc = "";
+        int numCurv = 0;
+    }
+}
