a70bc70bd0f9a4fb43179d18826f10e25c6d89bc
Oscar <oscarfuersuarez27@gmail.com>
2023-05-04 11:09:57 +0200

    cuarto paso

--- Pais.java
+++ Pais.java
@@ -8,26 +8,17 @@
 public class Pais
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private String nombrePais;
+    private String continentePais;
 
     /**
      * Constructor for objects of class Pais
      */
-    public Pais()
+    public Pais(String nomP, String contp)
     {
         // initialise instance variables
-        x = 0;
+        nombrePais = nomP;
+        continentePais = contp;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
