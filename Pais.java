60af82dd82750c5be6a3c30a646f8e7687fc9c3d
Marcos <martinezdiezmarcos7@gmail.com>
2023-05-11 09:17:58 +0200

    Metodos setter + imprimir detalles

--- Pais.java
+++ Pais.java
@@ -10,7 +10,7 @@ public class Pais
     // instance variables - replace the example below with your own
     private String nombrePais;
     private String continentePais;
-
+    private Pais pais;
     /**
      * Constructor for objects of class Pais
      */
@@ -23,5 +23,17 @@ public class Pais
 
      public String getNombrePais() {
         return nombrePais;
+    }
+    
+    public void setNombrePais(String nombrePais){
+        this.nombrePais = nombrePais;
+    }
+    
+    public void setContinentePais(String continentePais){
+        this.continentePais = continentePais;
+    }
+    
+    public String imprimirDetalles(){
+        return ("El nombre del pais es :" + nombrePais + "Y esta en el continente :" + continentePais);
     }
 }
